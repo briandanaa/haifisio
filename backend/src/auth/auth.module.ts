37c8f6997ad5564114dee8cdae43cd601182e220
Briandana Riznov <briandanariznov@gmail.com>
2026-03-10 05:07:56 +0700

    add security guard

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,16 +3,19 @@ import { AuthController } from './auth.controller'
 import { AuthService } from './auth.service'
 import { PrismaModule } from '../prisma/prisma.module'
 import { JwtModule } from '@nestjs/jwt'
+import { PassportModule } from '@nestjs/passport'
+import { JwtStrategy } from './jwt.strategy'
 
 @Module({
-  imports: [
-    PrismaModule,
-    JwtModule.register({
-      secret: 'secret',
-      signOptions: { expiresIn: '1d' }
-    })
-  ],
-  controllers: [AuthController],
-  providers: [AuthService],
+ imports: [
+  PrismaModule,
+  PassportModule,
+  JwtModule.register({
+   secret: process.env.JWT_SECRET,
+   signOptions: { expiresIn: '7d' },
+  }),
+ ],
+ controllers: [AuthController],
+ providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
